45c08f9f395ec25b765a39bac566ac75ace945c8
BUGHANTER <112425215+4ndr130ne@users.noreply.github.com>
2022-11-27 11:17:05 +0700

    Initialize update-uuid

---
+++ src/templates/programs.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Article from '../components/article/article'
+import programsNav from '../pages/programs/programs-nav.yaml'
+
+export default function Template({ data }) {
+  const { markdownRemark } = data
+  return (
+    <Article
+      links={programsNav}
+      path={markdownRemark.frontmatter.path}
+      title={markdownRemark.frontmatter.title}
+      description={markdownRemark.frontmatter.description}
+      docOnGithub={`${markdownRemark.frontmatter.id}.md`}
+    >
+      <h1>{markdownRemark.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
+    </Article>
+  )
+}
+
+export const pageQuery = graphql`
+  query programsDocByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        id
+        title
+        description
+      }
+    }
+  }
+`
