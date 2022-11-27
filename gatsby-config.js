185afa4087cc41649dadb65e68a9e8bdc01a3665
BUGHANTER <112425215+4ndr130ne@users.noreply.github.com>
2022-11-27 11:17:20 +0700

    Initialize disclosure-for-private-programs

---
+++ gatsby-config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  siteMetadata: {
+    title: 'HackerOne Platform Documentation',
+    siteUrl: 'https://docs.hackerone.com',
+    description: 'Get instant answers to the most common questions and learn how to use HackerOne.',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/docs/`,
+        name: "docs",
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sri',
+      options: {
+        hash: 'sha384'
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-49905813-10",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 500,
+              sizeByPixelDensity: true,
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
