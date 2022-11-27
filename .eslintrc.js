bb3cc9ae2bc83e2bb35a7e825cf671248ecc6702
BUGHANTER <112425215+4ndr130ne@users.noreply.github.com>
2022-11-27 11:17:58 +0700

    Initialize martijnrusschen-patch-9

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "plugins": [
+    "react",
+  ],
+  "globals": {
+    "graphql": false,
+  },
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true,
+    },
+  }
+}
