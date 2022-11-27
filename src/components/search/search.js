a14da92bc6704af2b622676fd3593f7f3d023031
BUGHANTER <112425215+4ndr130ne@users.noreply.github.com>
2022-11-27 11:17:28 +0700

    Initialize hacker-calendar-sync

---
+++ src/components/search/search.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import './search.scss'
+import './algolia.css'
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      enabled: true,
+    }
+  }
+
+  componentDidMount() {
+    if (window.docsearch) {
+      window.docsearch({
+        apiKey: 'acfb7def12803db2cd4ac0539b2b571a',
+        indexName: 'hackerone',
+        inputSelector: '#algolia-doc-search',
+      })
+    } else {
+      console.warn('Search has failed to load and now is being disabled')
+      this.setState({ enabled: false })
+    }
+  }
+
+  render() {
+    const { enabled } = this.state
+
+    return enabled ? (
+      <form className="search">
+        <input
+          className="search__input"
+          id="algolia-doc-search"
+          type="search"
+          placeholder="Search docs"
+          aria-label="Search docs"
+        />
+      </form>
+    ) : null
+  }
+}
+
+export default Search
