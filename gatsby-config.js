cf36316c2fa1f39ac388cb0d05c9e08113b11e5d
BUGHANTER <112425215+4ndr130ne@users.noreply.github.com>
2022-11-27 11:17:59 +0700

    Initialize martijnrusschen-patch-10

---
+++ gatsby-config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteMetadata: {
+    title: 'HackerOne Platform Documentation',
+    siteUrl: 'https://docs.hackerone.com',
+    description: 'Get instant answers to the most common questions and learn how to use HackerOne.',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-layout',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/docs/`,
+        name: "docs",
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sri',
+      options: {
+        hash: 'sha384'
+      },
+    },
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        mergeScriptHashes: false,
+        mergeStyleHashes: false,
+        mergeDefaultDirectives: true,
+        directives: {
+          "script-src": "'self' 'unsafe-eval' 'unsafe-inline' www.google-analytics.com cdn.jsdelivr.net *.algolia.net *.algolianet.com",
+          "style-src": "'self' 'unsafe-inline' cdn.jsdelivr.net",
+          "connect-src": "'self' www.google-analytics.com fbhzv4f2nk7b.statuspage.io *.algolia.net *.algolianet.com",
+          "frame-src": "www.youtube-nocookie.com"
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-49905813-10",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+        forceSSL: true,
+        transport: "beacon",
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-autolink-headers',
+            options: {
+              offsetY: 68,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 500
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-algolia-docsearch',
+      options: {
+        apiKey: "acfb7def12803db2cd4ac0539b2b571a",
+        indexName: "hackerone",
+        inputSelector: "#algolia-doc-search",
+      },
+    },
+  ],
+};
